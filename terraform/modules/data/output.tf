ffa6653c221b107e84987e646b2b536dbca8c0c3
Markin Sergey <ssmarkin.it@gmail.com>
2022-05-16 11:54:12 +0300

    provided caller indentity

--- terraform/modules/data/output.tf
+++ terraform/modules/data/output.tf
@@ -1,3 +1,11 @@
 output "aws_availability_zones" {
     value = data.aws_availability_zones.available
 }
+
+output "caller" {
+  value =  {
+      id = data.aws_caller_identity.current.account_id,
+      arn = data.aws_caller_identity.current.arn
+      user_id =  data.aws_caller_identity.current.user_id
+    }
+}
