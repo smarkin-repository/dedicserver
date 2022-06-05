43887034dbdaf77edafcaccc8a14ce1485208bde
Markin Sergey <ssmarkin.it@gmail.com>
2022-06-06 01:06:54 +0300

    improve scripts

--- Makefile
+++ Makefile
@@ -9,6 +9,9 @@ rollout:
 	@echo "This will run second, because it depends on other_file"
 	ls ${BUILD_DIR}
 
+all-resources:
+	aws resourcegroupstaggingapi get-resources --tag-filters Key=Stack,Values=xonotic --query "ResourceTagMappingList[].ResourceARN" --output json
+
 destroy:
 	echo "This will run first"
 	touch other_file
--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ aws eks --region us-west-2 update-kubeconfig --name xonotic-usw2-dev-eks --alias
 
 kubectl get nodes
 kubectl get svc
+
+# print all AWS resources with tag "xonotic-usw2-dev-eks" 
+aws resourcegroupstaggingapi get-resources --tag-filters Key=Stack,Values=xonotic --query "ResourceTagMappingList[].ResourceARN" --output json
